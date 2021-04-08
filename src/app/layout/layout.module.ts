d425576af8024cc7e5e7af71724dfa7cf9fb96ae
Niraj Kumar <Niraj.Kumar@citiustech.com>
2021-04-08 13:26:19 +0530

    Created user profile component

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -5,10 +5,11 @@ import { LayoutRoutingModule } from './layout-routing.module';
 import { HeaderComponent } from './pages/header/header.component';
 import { FooterComponent } from './pages/footer/footer.component';
 import { LayoutComponent } from './layout.component';
+import { UserProfileComponent } from './pages/user-profile/user-profile.component';
 
 
 @NgModule({
-  declarations: [LayoutComponent, HeaderComponent, FooterComponent],
+  declarations: [LayoutComponent, HeaderComponent, FooterComponent, UserProfileComponent],
   imports: [
     CommonModule,
     LayoutRoutingModule
