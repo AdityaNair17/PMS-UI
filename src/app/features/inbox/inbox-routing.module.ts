d699428f3479307bcc687e094efc9356eaac833e
Niraj Kumar <Niraj.Kumar@citiustech.com>
2021-04-08 13:50:19 +0530

    Created route for inbox module routing

--- src/app/features/inbox/inbox-routing.module.ts
+++ src/app/features/inbox/inbox-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InboxComponent } from './inbox.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: InboxComponent,
+    children: []
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/features/inbox/inbox.module.ts
+++ src/app/features/inbox/inbox.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { InboxRoutingModule } from './inbox-routing.module';
-import { InboxComponent } from './inbox/inbox.component';
+import { InboxComponent } from './inbox.component';
 
 
 @NgModule({
