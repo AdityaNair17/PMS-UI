dbe1c826e7b6bb5f967bc7647474d948ff3b1c46
Aditya Nair <AdityaN@citiustech.com>
2021-04-09 12:35:29 +0530

    Added Login logic

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,71 @@
+import { IUser } from './models/user-model';
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private authenticationToken : string = "";
+  private userRole : string = "";
+  private isUserAuthenticated : boolean = false;
+  private personalDetailsRequired : boolean = true;
+  private changePasswordRequired : boolean = true;
+  private user : IUser;
+
+  constructor(private http : HttpClient) { }
+
+  public get AuthenticationToken(){
+    return this.authenticationToken;
+  }
+
+  public set AuthenticationToken(authenticationToken : string){
+    this.authenticationToken = authenticationToken;
+  }
+
+  public get UserRole() {
+    return this.userRole;
+  }
+
+  public set UserRole(userRole : string) {
+    this.userRole = userRole;
+  }
+
+  public get IsUserAuthenticated(){
+    return this.isUserAuthenticated;
+  }
+
+  public set IsUserAuthenticated(isUserAuthenticated : boolean){
+    this.isUserAuthenticated = isUserAuthenticated;
+  }
+
+  public get PersonalDetailsRequired(){
+    return this.personalDetailsRequired;
+  }
+
+  public set PersonalDetailsRequired(personalDetailsRequired : boolean){
+    this.personalDetailsRequired = personalDetailsRequired;
+  }
+
+  public get ChangePasswordRequired(){
+    return this.changePasswordRequired;
+  }
+
+  public set ChangePasswordRequired(changePasswordRequired : boolean){
+    this.changePasswordRequired = changePasswordRequired;
+  }
+
+  public get User(){
+    return this.user;
+  }
+
+  public set User(user : IUser){
+    this.user = user;
+  }
+
+  public Login(user : any) : Observable<any> {
+    return this.http.post('http://localhost:300/login', user);
+  }
 }
--- src/app/auth/pages/sign-in/sign-in.component.ts
+++ src/app/auth/pages/sign-in/sign-in.component.ts
@@ -1,4 +1,6 @@
+import { AuthService } from './../../auth.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  public loginDetails : any = {
+    "userName" : "",
+    "password" : ""
+  }
+  constructor(private authSvc : AuthService, private router : Router) { }
 
   ngOnInit(): void {
   }
 
+  login() {
+    this.authSvc.Login(this.loginDetails).subscribe(
+      data => {
+        this.authSvc.AuthenticationToken = data.token;
+        this.authSvc.UserRole = data.user.role;
+        this.authSvc.ChangePasswordRequired = data.changePasswordRequired;
+        this.authSvc.PersonalDetailsRequired = data.personalDetailsRequired;
+        let user = {
+          firstName : data.user.firstName,
+          lastName : data.user.lastName,
+          emailId : data.user.emailId,
+          dateOfBirth : data.user.dateOfBirth
+        }
+        this.authSvc.User = user;
+        if(data.changePasswordRequired){
+          this.router.navigate(['\reset-password']);
+        } else if(data.personalDetailsRequired){
+          //re direct to patient details page
+          // this.router.navigate(['\'])
+        } else {
+          // redirect to inbox
+        }
+      }
+    );
+  }
 }
