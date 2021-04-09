6979c057b6468e81a294930a3a3f15ba085c0464
Niraj Kumar <Niraj.Kumar@citiustech.com>
2021-04-09 16:59:00 +0530

    Created routing for scheduler module

--- src/app/features/scheduler/scheduler.module.ts
+++ src/app/features/scheduler/scheduler.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SchedulerRoutingModule } from './scheduler-routing.module';
-import { SchedulerComponent } from './scheduler/scheduler.component';
+import { SchedulerComponent } from './scheduler.component';
 
 
 @NgModule({
