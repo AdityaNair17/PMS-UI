5148c2d735081e318555428b4ec873820f4feb42
Niraj Kumar <Niraj.Kumar@citiustech.com>
2021-04-09 16:57:36 +0530

    Added home module and added all lazyloaded routing in home module for features module

---
+++ src/app/home/home-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'inbox'
+      },
+      {
+        path: 'inbox',
+        loadChildren: () => import('../features/inbox/inbox.module').then(m => m.InboxModule)
+      },
+      {
+        path: 'scheduler',
+        loadChildren: () => import('../features/scheduler/scheduler.module').then(m => m.SchedulerModule)
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HomeRoutingModule { }
