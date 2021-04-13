f4261a002a3b376dedac07b9d08f0d547ff01767
Niraj Kumar <Niraj.Kumar@citiustech.com>
2021-04-13 15:41:26 +0530

    imported all component in inbox module

--- src/app/features/inbox/inbox.module.ts
+++ src/app/features/inbox/inbox.module.ts
@@ -3,13 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { InboxRoutingModule } from './inbox-routing.module';
 import { InboxComponent } from './inbox.component';
+import { InboxFilterComponent } from './components/inbox-filter/inbox-filter.component';
+import { InboxListComponent } from './components/inbox-list/inbox-list.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { InboxContainerComponent } from './components/inbox-container/inbox-container.component';
 
 
 @NgModule({
-  declarations: [InboxComponent],
+  declarations: [
+    InboxComponent, 
+    InboxFilterComponent, 
+    InboxListComponent, InboxContainerComponent],
   imports: [
     CommonModule,
-    InboxRoutingModule
+    InboxRoutingModule,
+    SharedModule
   ]
 })
 export class InboxModule { }
