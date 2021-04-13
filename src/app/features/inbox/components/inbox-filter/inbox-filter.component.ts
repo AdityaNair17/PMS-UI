01abfd6ad63dff19d8a5824fd68ea573e0efdc9e
Niraj Kumar <Niraj.Kumar@citiustech.com>
2021-04-13 15:39:35 +0530

    Created inbox filter

---
+++ src/app/features/inbox/components/inbox-filter/inbox-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inbox-filter',
+  templateUrl: './inbox-filter.component.html',
+  styleUrls: ['./inbox-filter.component.scss']
+})
+export class InboxFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
