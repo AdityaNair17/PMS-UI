3f250f949e11765b54c5ccd843c38483b47a90d5
Niraj Kumar <Niraj.Kumar@citiustech.com>
2021-04-13 15:40:54 +0530

    Created method getAllInboxByuser in inbox module

--- src/app/features/inbox/inbox.service.ts
+++ src/app/features/inbox/inbox.service.ts
@@ -1,9 +1,64 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { IInbox, STATUS } from './models/inbox-models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InboxService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getAllInboxByuser(user: string): Observable<IInbox[]> {
+    const inboxList: IInbox[] = [
+      {
+        id: 'qqahshjjk55612bdbfstjbsvfaayajbx',
+        name: 'ankit',
+        type: 'appointment/others',
+        subject: 'upcoming appointment',
+        status: STATUS.Upcoming,
+        time: '20:15',
+        date: '12 Apr'
+      },
+      {
+        id: 'qqahshjjk55612bdbfstjbsvfaayajbx',
+        name: 'ankit',
+        type: 'appointment/others',
+        subject: 'upcoming appointment',
+        status: STATUS.Upcoming,
+        time: '20:15',
+        date: '12 Apr'
+      },
+      {
+        id: 'qqahshjjk55612bdbfstjbsvfaayajbx',
+        name: 'ankit',
+        type: 'appointment/others',
+        subject: 'upcoming appointment',
+        status: STATUS.Upcoming,
+        time: '20:15',
+        date: '12 Apr'
+      },
+      {
+        id: 'qqahshjjk55612bdbfstjbsvfaayajbx',
+        name: 'ankit',
+        type: 'appointment/others',
+        subject: 'upcoming appointment',
+        status: STATUS.Upcoming,
+        time: '20:15',
+        date: '12 Apr'
+      },
+      {
+        id: 'qqahshjjk55612bdbfstjbsvfaayajbx',
+        name: 'ankit',
+        type: 'appointment/others',
+        subject: 'upcoming appointment',
+        status: STATUS.Upcoming,
+        time: '20:15',
+        date: '12 Apr'
+      }
+    ];
+    return of(inboxList);
+  }
+
 }
