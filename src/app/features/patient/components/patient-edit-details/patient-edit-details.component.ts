3ab3c6cb847828cbb6ae4745fb4f9c9e27e0f299
Niraj Kumar <Niraj.Kumar@citiustech.com>
2021-04-19 13:21:38 +0530

    Created patient-edit-details component and updated patient module

---
+++ src/app/features/patient/components/patient-edit-details/patient-edit-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-edit-details',
+  templateUrl: './patient-edit-details.component.html',
+  styleUrls: ['./patient-edit-details.component.scss']
+})
+export class PatientEditDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/features/patient/patient.module.ts
+++ src/app/features/patient/patient.module.ts
@@ -5,10 +5,11 @@ import { PatientRoutingModule } from './patient-routing.module';
 import { PatientComponent } from './patient.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { PatientDetailsComponent } from './components/patient-details/patient-details.component';
+import { PatientEditDetailsComponent } from './components/patient-edit-details/patient-edit-details.component';
 
 
 @NgModule({
-  declarations: [PatientComponent, PatientDetailsComponent],
+  declarations: [PatientComponent, PatientDetailsComponent, PatientEditDetailsComponent],
   imports: [
     CommonModule,
     PatientRoutingModule,
