e5ebfa40c27e9e3e32ecce926b3c7ddb6636c8f7
Niraj Kumar <Niraj.Kumar@citiustech.com>
2021-04-19 13:29:16 +0530

    Created patient-details component and updated module

--- src/app/features/patient/patient.module.ts
+++ src/app/features/patient/patient.module.ts
@@ -6,10 +6,11 @@ import { PatientComponent } from './patient.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { PatientDetailsComponent } from './components/patient-details/patient-details.component';
 import { PatientEditDetailsComponent } from './components/patient-edit-details/patient-edit-details.component';
+import { ViewPatientDetailsComponent } from './components/components/view-patient-details/view-patient-details.component';
 
 
 @NgModule({
-  declarations: [PatientComponent, PatientDetailsComponent, PatientEditDetailsComponent],
+  declarations: [PatientComponent, PatientDetailsComponent, PatientEditDetailsComponent, ViewPatientDetailsComponent],
   imports: [
     CommonModule,
     PatientRoutingModule,
