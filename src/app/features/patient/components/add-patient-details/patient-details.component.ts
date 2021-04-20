2cce261434eb4825949d0968351a924cb43f259b
Niraj Kumar <Niraj.Kumar@citiustech.com>
2021-04-20 18:13:33 +0530

    Created form control and UI of add patient details

--- src/app/features/patient/components/add-patient-details/patient-details.component.ts
+++ src/app/features/patient/components/add-patient-details/patient-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastMessageService } from 'src/app/shared/components/toast/service/toastMessage.service';
+import { accessList, genderList, languages } from 'src/app/shared/constants/constants';
 
 @Component({
   selector: 'app-patient-details',
@@ -6,10 +10,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./patient-details.component.scss']
 })
 export class PatientDetailsComponent implements OnInit {
+  genderList: string[] = genderList;
+  accessList: any[] = accessList;
+  languages: string[] = languages;
+  addPatientDetailsForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private toastMessageSvc: ToastMessageService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.buildForm();
+  }
+
+  buildForm() {
+    this.addPatientDetailsForm = this.formBuilder.group({
+      basicDetails: this.formBuilder.group({
+        firstName: [null, [Validators.required,]],
+        lastName: [null, [Validators.required,]],
+        emailId: [null, [Validators.required,]],
+        dateOfBirth: [null, [Validators.required,]],
+        contactNo: [null, [Validators.required,]],
+        age: [null, [Validators.required,]],
+        gender: [null, [Validators.required,]],
+        race: [null],
+        ethnicity: [null],
+      }),
+      address: this.formBuilder.group({
+        landmarkArea: [null, [Validators.required,]],
+        city: [null, [Validators.required,]],
+        state: [null, [Validators.required,]],
+        country: [null, [Validators.required,]],
+        pin: [null, [Validators.required,]],
+        addressType: [null, [Validators.required]]
+      }),
+      emergency_Details: this.formBuilder.group({
+        emergency_first_name: [null, [Validators.required,]],
+        emergency_last_name: [null, [Validators.required,]],
+        emergency_relation_ship: [null, [Validators.required,]],
+        emergency_contact_number: [null, [Validators.required,]],
+        _access_approved: [null, [Validators.required,]],
+        emergencyContactEmailId: [null, [Validators.required]],
+        addressType: [null, [Validators.required]]
+      }),
+      languageKnown: [[], [Validators.required]],
+      allergies: [[], [Validators.required]]
+    });
+  }
+
+  onAddPatientDetails(){
+    console.log(this.addPatientDetailsForm.value)
   }
 
 }
--- src/app/shared/constants/constants.ts
+++ src/app/shared/constants/constants.ts
@@ -1,17 +1,26 @@
 export const pmsConstants = {
-  invalidLoginMessage : 'Invalid EmailId or Password',
-  requiredFieldMessage : 'The above field is required',
-  TOAST_MESSAGE_TIMEOUT : 1000
+  invalidLoginMessage: 'Invalid EmailId or Password',
+  requiredFieldMessage: 'The above field is required',
+  TOAST_MESSAGE_TIMEOUT: 1000
 }
 
 export const toastErrMessage = {
-  severity : "error",
-  summary : "Error",
-  detail : "Something went wrong :("
+  severity: "error",
+  summary: "Error",
+  detail: "Something went wrong :("
 }
 
 export const toastSuccMessage = {
-  severity : "success",
-  summary : "Success",
-  detail : ""
+  severity: "success",
+  summary: "Success",
+  detail: ""
 }
+
+export const genderList: string[] = ['Male', 'Female'];
+
+export const accessList = [
+  { name: 'Yes', value: true },
+  { name: 'No', value: false }
+];
+
+export const languages = ['Hindi', 'English', 'Mandarin Chinese', 'Spanish', 'Russian'];
