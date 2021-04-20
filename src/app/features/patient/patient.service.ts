c300f57679a43c38c2d8019d12e125f0fbe9a42e
Niraj Kumar <Niraj.Kumar@citiustech.com>
2021-04-20 18:22:16 +0530

    addPatientDetils method in patient service

--- src/app/features/patient/patient.service.ts
+++ src/app/features/patient/patient.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { IPatientDetailsRes } from './models/patientDetails-model';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,8 @@ import { Injectable } from '@angular/core';
 export class PatientService {
 
   constructor() { }
+
+  addPatientDetils(patientDetails: any): Observable<IPatientDetailsRes> {
+    return of({ id: '456fdsdfhjjtrdsaw35789', status: 200, message: 'password changed Successfully' })
+  }
 }
