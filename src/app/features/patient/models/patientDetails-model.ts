bebeda13ef27251268d85d35660fd87d1fb82198
Niraj Kumar <Niraj.Kumar@citiustech.com>
2021-04-22 11:49:11 +0530

    Created  ILanguageKnown and IAllergies model

--- src/app/features/patient/models/patientDetails-model.ts
+++ src/app/features/patient/models/patientDetails-model.ts
@@ -3,3 +3,14 @@ import { IResponse } from "src/app/auth/models/masterResObj-model";
 export interface IPatientDetailsRes extends IResponse {
 id?:string;
 }
+
+export interface ILanguageKnown{
+    id?: number;
+    name?: string;
+}
+
+export interface IAllergies{
+    id?: number;
+    type?: string;
+    isFatal?: boolean;
+}
