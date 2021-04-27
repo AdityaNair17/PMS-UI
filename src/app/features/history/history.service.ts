38598181fb2424d076ad30245897f6c43f0f1bac
Niraj Kumar <Niraj.Kumar@citiustech.com>
2021-04-27 16:59:24 +0530

    Created getAllHistory method in history service

--- src/app/features/history/history.service.ts
+++ src/app/features/history/history.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { IHistoryList } from './models/history-model';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,17 @@ import { Injectable } from '@angular/core';
 export class HistoryService {
 
   constructor() { }
+
+  getAllHistory(userId?: string) : Observable<IHistoryList[]>{
+    const data: IHistoryList[] = [
+      {name:'Ankit Sawant', appointmentDate: '12/03/2021', diagnosis: ['abc'], procedure: ['xyz'], nextAppointmentDate: '29/03/2021'},
+      {name:'Ankit Sawant', appointmentDate: '12/03/2021', diagnosis: ['abc'], procedure: ['xyz'], nextAppointmentDate: '29/03/2021'},
+      {name:'Ankit Sawant', appointmentDate: '12/03/2021', diagnosis: ['abc'], procedure: ['xyz'], nextAppointmentDate: '29/03/2021'},
+      {name:'Ankit Sawant', appointmentDate: '12/03/2021', diagnosis: ['abc'], procedure: ['xyz'], nextAppointmentDate: '29/03/2021'},
+      {name:'Ankit Sawant', appointmentDate: '12/03/2021', diagnosis: ['abc'], procedure: ['xyz'], nextAppointmentDate: '29/03/2021'},
+      {name:'Ankit Sawant', appointmentDate: '12/03/2021', diagnosis: ['abc'], procedure: ['xyz'], nextAppointmentDate: '29/03/2021'},
+      {name:'Ankit Sawant', appointmentDate: '12/03/2021', diagnosis: ['abc'], procedure: ['xyz'], nextAppointmentDate: '29/03/2021'},
+    ];
+    return of(data);
+  }
 }
