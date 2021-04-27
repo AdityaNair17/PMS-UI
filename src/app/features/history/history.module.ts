8eecbd0e3e37d8f77646a69f61b3e1cfbaffe05b
Niraj Kumar <Niraj.Kumar@citiustech.com>
2021-04-27 16:22:47 +0530

    Created history container component

--- src/app/features/history/history.module.ts
+++ src/app/features/history/history.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { HistoryRoutingModule } from './history-routing.module';
 import { HistoryComponent } from './history.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { HistoryContainerComponent } from './history-container/history-container.component';
 
 
 @NgModule({
-  declarations: [HistoryComponent],
+  declarations: [HistoryComponent, HistoryContainerComponent],
   imports: [
     CommonModule,
     HistoryRoutingModule,
