c9aa9060dede17fc42ad81a0375b8ebed9b97e7d
Niraj Kumar <Niraj.Kumar@citiustech.com>
2021-04-27 17:17:23 +0530

    Created patient-visit component

--- src/app/features/patient-visit/patient-visit.module.ts
+++ src/app/features/patient-visit/patient-visit.module.ts
@@ -5,13 +5,21 @@ import { PatientVisitRoutingModule } from './patient-visit-routing.module';
 import { VisitMedicationComponent } from './visit-medication/visit-medication.component';
 import { VisitDiagnosisComponent } from './visit-diagnosis/visit-diagnosis.component';
 import { VisitProcedureComponent } from './visit-procedure/visit-procedure.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { PatientVisitComponent } from './patient-visit.component';
 
 
 @NgModule({
-  declarations: [VisitMedicationComponent, VisitDiagnosisComponent, VisitProcedureComponent],
+  declarations: [
+    VisitMedicationComponent, 
+    VisitDiagnosisComponent, 
+    VisitProcedureComponent, 
+    PatientVisitComponent
+  ],
   imports: [
     CommonModule,
-    PatientVisitRoutingModule
+    PatientVisitRoutingModule,
+    SharedModule
   ]
 })
 export class PatientVisitModule { }
