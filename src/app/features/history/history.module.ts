d221c082f34513f80a66bce90e64d48cc042f3fc
Niraj Kumar <Niraj.Kumar@citiustech.com>
2021-04-27 16:23:02 +0530

    Created history list component

--- src/app/features/history/history.module.ts
+++ src/app/features/history/history.module.ts
@@ -5,10 +5,11 @@ import { HistoryRoutingModule } from './history-routing.module';
 import { HistoryComponent } from './history.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { HistoryContainerComponent } from './history-container/history-container.component';
+import { HistoryListComponent } from './history-list/history-list.component';
 
 
 @NgModule({
-  declarations: [HistoryComponent, HistoryContainerComponent],
+  declarations: [HistoryComponent, HistoryContainerComponent, HistoryListComponent],
   imports: [
     CommonModule,
     HistoryRoutingModule,
